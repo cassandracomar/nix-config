1684260ac077ce724f7b22be5b2f1559010638bc
Cassandra Comar <cassandra@ditto.live>
2022-04-01 19:00:50 -0400

    *sigh*

--- system/base/docker.nix
+++ system/base/docker.nix
@@ -7,7 +7,7 @@
       storageDriver = "overlay2";
       daemon.settings = {
         bip = "172.16.0.100/24";
-        #dns = [ "127.0.0.11" ];
+        dns = [ "127.0.0.11" ];
       };
     };
     libvirtd.enable = true;
