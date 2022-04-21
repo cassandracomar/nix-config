e66cc7fc9a8df708cce7dcb9977bf54e317e17d9
Cassandra Comar <cassandra@ditto.live>
2022-04-21 09:01:35 -0400

    fix editor and visual

--- users/base/environment.nix
+++ users/base/environment.nix
@@ -5,8 +5,8 @@
   home.sessionVariables._JAVA_AWT_WM_NONREPARENTING = "1";
   home.sessionVariables.MOZ_ACCELERATED = "1";
   home.sessionVariables.MOZ_WEBRENDER = "1";
-  home.sessionVariables.EDITOR = "emacsclient -t";
-  home.sessionVariables.VISUAL = "emacsclient -c";
+  home.sessionVariables.EDITOR = "${pkgs.emacsPgtkGcc}/bin/emacsclient -t";
+  home.sessionVariables.VISUAL = "${pkgs.emacsPgtkGcc}/bin/emacsclient -c";
   home.sessionVariables.ALTERNATE = "vim";
   home.sessionVariables.XDG_DATA_DIRS =
     "$HOME/.nix-profile/share:$XDG_DATA_DIRS";
