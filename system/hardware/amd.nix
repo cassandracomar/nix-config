020833e5843313945fe30040a5b18ee4fc460eaf
Cassandra Comar <cassandra@ditto.live>
2022-08-31 13:24:43 -0400

    fix brightness device name

--- system/hardware/amd.nix
+++ system/hardware/amd.nix
@@ -14,8 +14,8 @@
       event = "video/brightnessdown";
       action = ''
         #!${pkgs.bash}/bin/bash
-        ${pkgs.brightnessctl}/bin/brightnessctl -d amdgpu_bl1 s 5%-
-        raw=$(${pkgs.brightnessctl}/bin/brightnessctl -d amdgpu_bl1 g)
+        ${pkgs.brightnessctl}/bin/brightnessctl -d amdgpu_bl0 s 5%-
+        raw=$(${pkgs.brightnessctl}/bin/brightnessctl -d amdgpu_bl0 g)
         perc=$(${pkgs.bc}/bin/bc <<< "scale=4; 100*$raw/255")
         /run/wrappers/bin/sudo -u cassandra DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus ${pkgs.libnotify}/bin/notify-send " " -i display-brightness-symbolic.symbolic -h int:value:$perc -h string:x-canonical-private-synchronous:brightness &
       '';
@@ -24,8 +24,8 @@
       event = "video/brightnessup";
       action = ''
         #!${pkgs.bash}/bin/bash
-        ${pkgs.brightnessctl}/bin/brightnessctl -d amdgpu_bl1 s 5%+
-        raw=$(${pkgs.brightnessctl}/bin/brightnessctl -d amdgpu_bl1 g)
+        ${pkgs.brightnessctl}/bin/brightnessctl -d amdgpu_bl0 s 5%+
+        raw=$(${pkgs.brightnessctl}/bin/brightnessctl -d amdgpu_bl0 g)
         perc=$(${pkgs.bc}/bin/bc <<< "scale=4; 100*$raw/255")
         /run/wrappers/bin/sudo -u cassandra DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus ${pkgs.libnotify}/bin/notify-send " " -i display-brightness-symbolic.symbolic -h int:value:$perc -h string:x-canonical-private-synchronous:brightness &
       '';
