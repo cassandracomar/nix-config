d924f8b50003f7c291c69a04ceca19a35e994f71
Cassandra Comar <cassandra@ditto.live>
2022-10-07 22:25:26 -0400

    use tlp settings instead of extraConfig

--- system/base/power-management.nix
+++ system/base/power-management.nix
@@ -3,10 +3,10 @@
 {
   services.tlp = {
     enable = true;
-    extraConfig = ''
-      CPU_SCALING_GOVERNOR_ON_AC=ondemand
-      CPU_SCALING_GOVERNOR_ON_BAT=schedutil
-    '';
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_AC = "ondemand";
+      CPU_SCALING_GOVERNOR_ON_BAT = "schedutil";
+    };
   };
   services.acpid.enable = true;
   powerManagement.enable = true;
