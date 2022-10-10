4a76fcf1d8111c9fe91a88762bab008d73e49fec
Cassandra Comar <cassandra@ditto.live>
2022-10-10 13:39:23 -0400

    hls is building too much

--- users/base/haskell.nix
+++ users/base/haskell.nix
@@ -4,7 +4,7 @@
   home.packages = with pkgs; [
     haskellPackages.yeganesh
     (pkgs.hiPrio haskellPackages.stack)
-    haskell-language-server
+    # haskell-language-server
     haskellPackages.cabal-install
     ghc
   ];
