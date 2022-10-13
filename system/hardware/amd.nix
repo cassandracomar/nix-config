ef7ed6ee37f2bb326fb314016bbd1d05c9ea286e
Cassandra Comar <cassandra@ditto.live>
2022-10-12 21:45:36 -0400

    try looking up the name of the backlight device

--- system/hardware/amd.nix
+++ system/hardware/amd.nix
@@ -36,8 +36,9 @@ in
       event = "video/brightnessdown";
       action = ''
         #!${pkgs.bash}/bin/bash
-        ${pkgs.brightnessctl}/bin/brightnessctl -d amdgpu_bl0 s 5%-
-        raw=$(${pkgs.brightnessctl}/bin/brightnessctl -d amdgpu_bl0 g)
+        bl=$(brightnessctl -l | grep amd | cut -d' ' -f 3)
+        ${pkgs.brightnessctl}/bin/brightnessctl -d $bl s 5%-
+        raw=$(${pkgs.brightnessctl}/bin/brightnessctl -d $bl g)
         perc=$(${pkgs.bc}/bin/bc <<< "scale=4; 100*$raw/255")
         /run/wrappers/bin/sudo -u cassandra DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus ${pkgs.libnotify}/bin/notify-send " " -i display-brightness-symbolic.symbolic -h int:value:$perc -h string:x-canonical-private-synchronous:brightness &
       '';
@@ -46,8 +47,9 @@ in
       event = "video/brightnessup";
       action = ''
         #!${pkgs.bash}/bin/bash
-        ${pkgs.brightnessctl}/bin/brightnessctl -d amdgpu_bl0 s 5%+
-        raw=$(${pkgs.brightnessctl}/bin/brightnessctl -d amdgpu_bl0 g)
+        bl=$(brightnessctl -l | grep amd | cut -d' ' -f 3)
+        ${pkgs.brightnessctl}/bin/brightnessctl -d $bl s 5%+
+        raw=$(${pkgs.brightnessctl}/bin/brightnessctl -d $bl g)
         perc=$(${pkgs.bc}/bin/bc <<< "scale=4; 100*$raw/255")
         /run/wrappers/bin/sudo -u cassandra DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus ${pkgs.libnotify}/bin/notify-send " " -i display-brightness-symbolic.symbolic -h int:value:$perc -h string:x-canonical-private-synchronous:brightness &
       '';
