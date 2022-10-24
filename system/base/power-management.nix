1a63266a48c36c07795e91e0f0c620140e6277ea
Cassandra Comar <cassandra@ditto.live>
2022-10-23 23:47:51 -0400

    set tlp default mode

--- system/base/power-management.nix
+++ system/base/power-management.nix
@@ -4,6 +4,7 @@
   services.tlp = {
     enable = true;
     settings = {
+      TLP_DEFAULT_MODE = "AC";
       CPU_SCALING_GOVERNOR_ON_AC = "ondemand";
       CPU_SCALING_GOVERNOR_ON_BAT = "schedutil";
     };
