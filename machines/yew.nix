9efb08385cc73a6cf6cb95cc57b036499e670b21
Cassandra Comar <cassandra@ditto.live>
2022-10-23 22:50:38 -0400

    add mount options

--- machines/yew.nix
+++ machines/yew.nix
@@ -28,6 +28,13 @@
   fileSystems."/" =
     { device = "/dev/disk/by-uuid/56bf7cf3-5c55-47bf-8aed-47be50a1f0b1";
       fsType = "f2fs";
+      options = [
+        "compress_algorithm=zstd"
+        "compress_chksum"
+        "atgc"
+        "gc_merge"
+        "lazytime"
+      ];
     };
 
   fileSystems."/boot" =
