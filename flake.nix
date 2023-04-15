138771273da168af4f1ebb74d455bebac75fad81
Cassandra Comar <cassandra@ditto.live>
2023-04-15 10:44:05 -0400

    fix hash

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
   inputs.home-manager.url = "github:nix-community/home-manager";
   inputs.home-manager.inputs.nixpkgs.follows = "nixpkgs";
   inputs.xmonad-personal.url = "github:cassandracomar/dotxmonad";
-  inputs.robotnix.url = "github:cassandracomar/robotnix";
+  inputs.robotnix.url = "github:cassandracomar/robotnix/fix-cts-profile";
 
   # encryption
   inputs.sops-nix.url = "github:Mic92/sops-nix";
