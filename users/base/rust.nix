ea4267a7a8bc832250d4c255b0767cddb6d252e8
Cassandra Comar <ccomar@drwholdings.com>
2025-08-18 07:40:36 -0400

    remove cargo-web

--- users/base/rust.nix
+++ users/base/rust.nix
@@ -1,17 +1,19 @@
-{ config, lib, pkgs, ... }:
-
-let
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
   rustChannel = pkgs.rust-bin.stable.latest;
   rustpkgs = rustChannel.default.override {
-    targets = [ "wasm32-unknown-unknown" ];
-    extensions = [ "rust-src" "clippy-preview" "rust-analysis" ];
+    targets = ["wasm32-unknown-unknown"];
+    extensions = ["rust-src" "clippy-preview" "rust-analysis"];
   };
 in {
   home.packages = with pkgs; [
     (pkgs.lowPrio rustpkgs)
     pkgs.rust-analyzer
     cargo-audit
-    cargo-web
     rustfmt
     gdb
     clang
