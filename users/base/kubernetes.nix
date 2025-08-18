cf55e62aae1e81e60b3a69c06316efd17aed9974
Cassandra Comar <ccomar@drwholdings.com>
2025-08-18 07:53:27 -0400

    use upstream kustomize

--- users/base/kubernetes.nix
+++ users/base/kubernetes.nix
@@ -1,40 +1,9 @@
-{ config, lib, pkgs, ... }:
-
-let
-  kustomize' = pkgs.buildGoModule rec {
-    pname = "kustomize";
-    version = "5.0.1";
-    src = pkgs.fetchFromGitHub {
-      owner = "kubernetes-sigs";
-      repo = pname;
-      rev = "kustomize/v${version}";
-      sha256 = "sha256-wVdB9fTLYg7Ma0dRgDt7X7ncN0+04DyT8kp2+/aQ018=";
-    };
-    vendorSha256 = "sha256-bY3TxRErkUEaBCSk0w7LhvLC10YDCJhPV73fKwlWuFI=";
-    GOWORK = "off";
-
-    ldflags = let t = "sigs.k8s.io/kustomize/api/provenance"; in
-      [
-        "-s"
-        "-X ${t}.version=${version}"
-        "-X ${t}.gitCommit=${src.rev}"
-      ];
-
-    # avoid finding test and development commands
-    modRoot = "kustomize";
-
-    nativeBuildInputs = [ pkgs.installShellFiles ];
-
-    postInstall = ''
-      installShellCompletion --cmd kustomize \
-        --bash <($out/bin/kustomize completion bash) \
-        --fish <($out/bin/kustomize completion fish) \
-        --zsh <($out/bin/kustomize completion zsh)
-    '';
-
-  };
-in
 {
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
   home.packages = with pkgs; [
     kubectl
     krew
@@ -57,6 +26,6 @@ in
     vault
     istioctl
     vcluster
-    kustomize'
+    kustomize
   ];
 }
