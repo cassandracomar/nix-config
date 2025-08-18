0e9949acfbe462492b859610acf3b49d9d80509d
Cassandra Comar <ccomar@drwholdings.com>
2025-08-18 07:56:01 -0400

    remove extra mentions of emacs29-pgtk

--- users/base/environment.nix
+++ users/base/environment.nix
@@ -7,8 +7,8 @@
   home.sessionVariables._JAVA_AWT_WM_NONREPARENTING = "1";
   home.sessionVariables.MOZ_ACCELERATED = "1";
   home.sessionVariables.MOZ_WEBRENDER = "1";
-  home.sessionVariables.EDITOR = "${pkgs.emacs29-pgtk}/bin/emacsclient -t";
-  home.sessionVariables.VISUAL = "${pkgs.emacs29-pgtk}/bin/emacsclient -c";
+  home.sessionVariables.EDITOR = "${pkgs.emacs-pgtk}/bin/emacsclient -t";
+  home.sessionVariables.VISUAL = "${pkgs.emacs-pgtk}/bin/emacsclient -c";
   home.sessionVariables.ALTERNATE = "vim";
   home.sessionVariables.XDG_DATA_DIRS = "$HOME/.nix-profile/share:$XDG_DATA_DIRS";
   home.sessionVariables.PATH = "$HOME/.cargo/bin:$HOME/.local/bin:$HOME/.doom-emacs.d/bin:$HOME/.emacs.d/bin:$HOME/.krew/bin:$PATH";
