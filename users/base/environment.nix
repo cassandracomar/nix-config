a9689fa7428a4ff5b0692c87ea53be1a25fea68d
Cassandra Comar <cass@ndra.io>
2025-08-26 18:48:06 -0400

    make sure user processes also pick it up

--- users/base/environment.nix
+++ users/base/environment.nix
@@ -1,12 +1,15 @@
 { pkgs, ... }: {
-  home.sessionVariables._JAVA_AWT_WM_NONREPARENTING = "1";
-  home.sessionVariables.MOZ_ACCELERATED = "1";
-  home.sessionVariables.MOZ_WEBRENDER = "1";
-  home.sessionVariables.MOZ_ENABLE_WAYLAND = "1";
-  home.sessionVariables.EDITOR = "${pkgs.emacs-pgtk}/bin/emacsclient -t";
-  home.sessionVariables.VISUAL = "${pkgs.emacs-pgtk}/bin/emacsclient -c";
-  home.sessionVariables.ALTERNATE = "vim";
-  home.sessionVariables.DRI_PRIME = "1";
+  home.sessionVariables = {
+    _JAVA_AWT_WM_NONREPARENTING = "1";
+    MOZ_ACCELERATED = "1";
+    MOZ_WEBRENDER = "1";
+    MOZ_ENABLE_WAYLAND = "1";
+    EDITOR = "${pkgs.emacs-pgtk}/bin/emacsclient -t";
+    VISUAL = "${pkgs.emacs-pgtk}/bin/emacsclient -c";
+    ALTERNATE = "vim";
+    DRI_PRIME = "1";
+    FREETYPE_PROPERTIES = "cff:no-stem-darkening=0 autofitter:no-stem-darkening=0";
+  };
   home.sessionPath = ["$HOME/.cargo/bin" "$HOME/.local/bin" "$HOME/krew/bin"];
   xdg.systemDirs.data = ["$HOME/.nix-profile/share"];
 
