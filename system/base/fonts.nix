ccb1b6b332279f05108aabd82573b1932d887784
Cassandra Comar <cass@ndra.io>
2025-08-26 17:43:58 -0400

    try disabling hinting

--- system/base/fonts.nix
+++ system/base/fonts.nix
@@ -24,7 +24,7 @@
 
       # Fixes antialiasing blur
       hinting = {
-        enable = true;
+        enable = false;
         style = "slight"; # no difference
         autohint = false; # no difference
       };
