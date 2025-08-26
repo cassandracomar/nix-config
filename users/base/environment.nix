120ac09aa73831b490affed4acdaf228dc435629
Cassandra Comar <cass@ndra.io>
2025-08-26 16:42:21 -0400

    add MOZ_ENABLE_WAYLAND

--- users/base/environment.nix
+++ users/base/environment.nix
@@ -2,6 +2,7 @@
   home.sessionVariables._JAVA_AWT_WM_NONREPARENTING = "1";
   home.sessionVariables.MOZ_ACCELERATED = "1";
   home.sessionVariables.MOZ_WEBRENDER = "1";
+  home.sessionVariables.MOZ_ENABLE_WAYLAND = "1";
   home.sessionVariables.EDITOR = "${pkgs.emacs-pgtk}/bin/emacsclient -t";
   home.sessionVariables.VISUAL = "${pkgs.emacs-pgtk}/bin/emacsclient -c";
   home.sessionVariables.ALTERNATE = "vim";
