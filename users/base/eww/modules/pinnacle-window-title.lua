9672f3edf4c48b5829968b5ce42bf8d2f1045801
Cassandra Comar <cass@ndra.io>
2025-08-28 17:49:30 -0400

    make window title update more often

--- users/base/eww/modules/pinnacle-window-title.lua
+++ users/base/eww/modules/pinnacle-window-title.lua
@@ -6,6 +6,24 @@ get_active_window_title(Window.get_focused())
 
 pinnacle.setup(function()
   Window.connect_signal({
-    focused = get_active_window_title
+    focused = function(win)
+      get_active_window_title(win)
+    end,
+    pointer_enter = function(win)
+      get_active_window_title(win)
+    end,
+    pointer_leave = function(win)
+      get_active_window_title(win)
+    end
+  })
+  Tag.connect_signal({
+    active = function(tag, active)
+      get_active_window_title(Window.get_focused())
+    end
+  })
+  Output.connect_signal({
+    focused = function(outp)
+      get_active_window_title(Window.get_focused())
+    end
   })
 end)
