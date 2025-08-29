9c8708ff1dec8c008679051dd0f30dc5dddbfa6c
Cassandra Comar <cass@ndra.io>
2025-08-29 08:05:21 -0400

    try adding a tab bar

---
+++ users/base/eww/modules/focus-window.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+pinnacle client <<-EOT
+local next = Window.handle.new("$1")
+local focused = Window.get_focused()
+
+if (focused ~= nil and focused:maximized()) then
+  focused:lower()
+  next:set_maximized(true)
+  next:raise()
+end
+
+next:set_focused(true)
+EOT
---
+++ users/base/eww/modules/pinnacle-tab-bar.lua
@@ -0,0 +1,99 @@
+local cjson = require("cjson")
+
+-- turn a table whos entries are themselves tables/arrays into a flat array
+-- i.e. [[1,2,3], [4, 5, 6]] -> [1, 2, 3, 4, 5, 6]
+-- this only removes a single level of nesting
+function flatten(table)
+  local count = 0
+  local res = {}
+  for k, v in ipairs(table) do
+    for i, e in ipairs(v) do
+      res[count] = e
+      count = count + 1
+    end
+  end
+
+  return res
+end
+
+-- get the windows on all active tags on the output
+function windows_for_output(output)
+  local tags = output.active_tags()
+  local windows = {}
+
+  for _, tag in ipairs(tags) do
+    windows[tag:name()] = tag:windows()
+  end
+
+  return flatten(windows)
+end
+
+-- get a table from the output name to the windows active on that output
+function windows_for_all_outputs()
+  local outputs = Output.get_all()
+  local windows = {}
+  for _, output in ipairs(outputs) do
+    windows[output.name] = windows_for_output(output)
+  end
+
+  return windows
+end
+
+-- turn the window into a tab bar button
+function make_tab(window)
+  local class = ':class "tab"'
+  local switch_to = ':onclick "./modules/focus-window.sh ' .. window.id .. '" '
+  local label = '(label :class "title" :show-truncated true "' .. window:title() .. '")'
+  local button = '(button ' .. class .. switch_to .. label ')'
+
+  return button
+end
+
+-- turn a list of windows into a tab bar, excluding the focused window
+function make_tab_bar(windows)
+  local tab_bar = '(box :class "tab-bar" :orientation "h" :space-evenly true'
+  for i, window in ipairs(windows) do
+    if (not window:focused()) then
+      tab_bar = tab_bar .. ' ' .. make_tab(window)
+    end
+  end
+
+  return tab_bar .. ')'
+end
+
+-- get tab bars for each output
+function get_tab_bars()
+  local windows_by_output = windows_for_all_outputs()
+  local tab_bars = {}
+  for outp, windows in ipairs(windows_by_output) do
+    tab_bars[outp] = make_tab_bar(windows)
+  end
+
+  return cjson.encode(tab_bars)
+end
+
+print(get_tab_bars())
+
+Pinnacle.setup(function ()
+  Window.connect_signal({
+    focused = function(win)
+      print(get_tab_bars())
+    end,
+    pointer_enter = function(win)
+      print(get_tab_bars())
+    end,
+    pointer_leave = function(win)
+      print(get_tab_bars())
+    end
+  })
+  Tag.connect_signal({
+      active = function(tag, active)
+        print(get_tab_bars())
+      end
+  })
+  Output.connect_signal({
+      focused = function(outp)
+        print(get_tab_bars())
+      end
+  })
+end)
