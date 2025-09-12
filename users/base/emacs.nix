c7cd8c7f5141c0bfe9f19596cb3fc5491e757e9b
Cassandra Comar <cass@ndra.io>
2025-09-11 20:13:42 -0400

    turn off profile generation

--- users/base/emacs.nix
+++ users/base/emacs.nix
@@ -23,7 +23,7 @@
         "-march=znver4"
         "-mtune=znver4"
         "-flto=thin"
-        "-fprofile-generate"
+        # "-fprofile-generate"
         "-fprofile-use=${profdata}"
       ]
       ++ old.NIX_CFLAGS_COMPILE or []);
