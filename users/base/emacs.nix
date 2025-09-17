a7eab9e31f36b072641b0e3576ac8020cd4f1877
Cassandra Comar <cass@ndra.io>
2025-09-17 10:40:54 -0400

    try switching back to emacs-pgtk

--- users/base/emacs.nix
+++ users/base/emacs.nix
@@ -6,7 +6,7 @@
   ...
 }: let
   profdata = ./merged.profdata;
-  emacs' = pkgs.emacs-igc-pgtk.overrideAttrs (old: {
+  emacs' = pkgs.emacs-pgtk.overrideAttrs (old: {
     stdenv = pkgs.llvmPackages.stdenv;
     preConfigure = ''
       export CC=${pkgs.llvmPackages.clang}/bin/clang
