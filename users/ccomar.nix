4e617bee0b4d6061e9c7ef700687042dd54cc5e6
Cassandra Comar <ccomar@drwholdings.com>
2025-10-02 11:04:05 -0400

    set reload triggers?

--- users/ccomar.nix
+++ users/ccomar.nix
@@ -52,7 +52,10 @@ in {
     pinnacle = config.lib.nixGL.wrap pkgs.pinnacle;
   in {
     Service.ExecStart = lib.mkForce "${pinnacle}/bin/pinnacle --session";
-    Unit.X-SwitchMethod = "reload";
+    Unit = {
+      X-SwitchMethod = "reload";
+      X-ReloadTriggers = ["${pinnacle}"];
+    };
   };
   xdg.systemDirs.data = ["/usr/share/ubuntu" "/usr/share/gnome" "/usr/local/share" "/usr/share"];
 
