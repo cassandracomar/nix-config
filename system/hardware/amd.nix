2d6d2af9800804f9588d80e213f504b9d1935554
Cassandra Comar <cass@ndra.io>
2025-10-21 10:30:51 -0400

    disable usb autosuspend

--- system/hardware/amd.nix
+++ system/hardware/amd.nix
@@ -45,7 +45,7 @@ in {
   boot.initrd.availableKernelModules = ["nvme" "xhci_pci" "uas" "usbhid" "sd_mod" "sdhci_pci"];
   boot.initrd.kernelModules = [];
   boot.kernelModules = ["amd_pstate" "kvm_amd" "cpuid" "i2c-dev" "zenpower" "corefreqk"];
-  boot.kernelParams = ["amdgpu.backlight=0" "acpi_backlight=video" "initcall_blacklist=acpi_cpufreq_init" "amd_pstate=active"];
+  boot.kernelParams = ["amdgpu.backlight=0" "acpi_backlight=video" "initcall_blacklist=acpi_cpufreq_init" "amd_pstate=active" "usbcore.autosuspend=-1"];
   boot.extraModulePackages = with config.boot.kernelPackages; [zenpower CoreFreq];
   boot.kernelPackages = pkgs-optimized.linuxKernel.packagesFor pkgs-optimized.linuxKernel.kernels.linux_xanmod_latest;
   # boot.kernelPackages = pkgs-optimized.linuxKernel.packagesFor (let
