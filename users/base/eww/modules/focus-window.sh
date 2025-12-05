578aafbce868dbb8f9cbf94951f2726c17c53b13
Cassandra Comar <cass@ndra.io>
2025-12-05 14:29:47 -0500

    remove Pinnacle.run

--- users/base/eww/modules/focus-window.sh
+++ users/base/eww/modules/focus-window.sh
@@ -1,16 +1,14 @@
 #!/usr/bin/env sh
 
 pinnacle client <<-EOT
-Pinnacle.run(function()
-  local next = Window.handle.new("$1")
-  local focused = Window.get_focused()
-  
-  if (focused ~= nil and focused:maximized()) then
-    focused:lower()
-    next:set_maximized(true)
-    next:raise()
-  end
-  
-  next:set_focused(true)
-end)
+local next = Window.handle.new("$1")
+local focused = Window.get_focused()
+
+if (focused ~= nil and focused:maximized()) then
+  focused:lower()
+  next:set_maximized(true)
+  next:raise()
+end
+
+next:set_focused(true)
 EOT
