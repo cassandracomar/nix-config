15816b9f3e83681f844e07eb9dfa543dca5d3fe5
Cassandra Comar <cass@ndra.io>
2025-12-05 13:58:25 -0500

    try nesting in pinnacle.run?

--- users/base/eww/modules/focus-window.sh
+++ users/base/eww/modules/focus-window.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env sh
 
 pinnacle client <<-EOT
-local next = Window.handle.new("$1")
-local focused = Window.get_focused()
-
-if (focused ~= nil and focused:maximized()) then
-  focused:lower()
-  next:set_maximized(true)
-  next:raise()
-end
-
-focused:swap(next)
-next:set_focused(true)
+pinnacle.run(function()
+  local next = Window.handle.new("$1")
+  local focused = Window.get_focused()
+  
+  if (focused ~= nil and focused:maximized()) then
+    focused:lower()
+    next:set_maximized(true)
+    next:raise()
+  end
+  
+  focused:swap(next)
+  next:set_focused(true)
+end)
 EOT
