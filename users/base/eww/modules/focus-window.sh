c1e0538a9d73e4aafb1485323a14c10784cfdf26
Cassandra Comar <cass@ndra.io>
2025-12-05 13:49:39 -0500

    set up a layout requester

--- users/base/eww/modules/focus-window.sh
+++ users/base/eww/modules/focus-window.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env sh
 
 pinnacle client <<-EOT
+local layout_requester = Layout.manage(function (layout_args)
+  local master_stack = require("pinnacle.layout").builtin.master_stack()
+  local root_node = master_stack:layout(layout_args.window_count)
+  local tree_id = layout_args.tags[1] and layout_args.tags[1].id or 0
+
+  return {
+    root_node = root_node,
+    tree_id = tree_id,
+  }
+end)
+
 local next = Window.handle.new("$1")
 local focused = Window.get_focused()
 
@@ -12,4 +23,5 @@ end
 
 focused:swap(next)
 next:set_focused(true)
+layout_requester:request_layout(nil)
 EOT
