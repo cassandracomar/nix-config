44dd47818009f5801aca8e102282d919bd366403
Cassandra Comar <cass@ndra.io>
2025-12-05 11:23:52 -0500

    add home-manager search

--- users/cassandra.nix
+++ users/cassandra.nix
@@ -201,6 +201,28 @@ in {
               definedAliases = ["@no"];
             };
 
+            home-manager-options = {
+              name = "home-manager options";
+              urls = [
+                {
+                  template = "https://home-manager-options.extranix.com";
+                  params = [
+                    {
+                      name = "release";
+                      value = "master";
+                    }
+                    {
+                      name = "query";
+                      value = "{searchTerms}";
+                    }
+                  ];
+                }
+              ];
+
+              icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+              definedAliases = ["@hm"];
+            };
+
             nixos-wiki = {
               name = "NixOS Wiki";
               urls = [{template = "https://wiki.nixos.org/w/index.php?search={searchTerms}";}];
