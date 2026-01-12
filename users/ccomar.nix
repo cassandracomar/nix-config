3cf5967c9b3008045b973759c72ad23035fd2168
Cassandra Comar <cass@ndra.io>
2026-01-12 14:56:56 -0500

    remove patches

--- users/ccomar.nix
+++ users/ccomar.nix
@@ -56,15 +56,6 @@
       ]
       ++ old.NIX_CFLAGS_COMPILE or []
     );
-
-    patches =
-      (old.patches or [])
-      ++ [
-        (pkgs.fetchpatch {
-          url = "https://lists.gnu.org/archive/html/bug-gnu-emacs/2025-09/txtvy4M7RzD_C.txt";
-          sha256 = "sha256-dV6NBGm0ryz069Iv/eSdRC/K/ZDYF+jp3ZUoGNZaeA8";
-        })
-      ];
   });
 in {
   imports = [./base];
