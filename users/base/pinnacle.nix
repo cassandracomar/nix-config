919a6ba8707a13726a053ec2f92d90568a8b05e9
Cassandra Comar <ccomar@drwholdings.com>
2026-01-16 11:01:09 -0500

    just use the first word for the app name

--- users/base/pinnacle.nix
+++ users/base/pinnacle.nix
@@ -83,7 +83,9 @@ in {
       sidebar-mode = true;
       monitor = -4;
       dpi = 96;
-      run-command = "bash -c '${pkgs.uwsm}/bin/uwsm app -a \\$(systemd-escape {cmd}) {cmd}'";
+      run-command = ''
+        bash -c '${pkgs.uwsm}/bin/uwsm app -a \\$(echo "{cmd}" | cut -d" " -f 1) {cmd}'
+      '';
     };
   };
 
