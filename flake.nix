2b66f6fbbae835de881d152b925862cc41acc886
Cassandra Comar <cass@ndra.io>
2026-01-15 21:13:26 -0500

    try setting zfs package another way?

--- flake.nix
+++ flake.nix
@@ -160,7 +160,7 @@
         kernelPackages = pkgs.lib.mkDefault pkgs.cachyosKernels.linux-cachyos-latest-lto-zen4;
         # bug fix for performance regression for zfs since 5.3
         kernelParams = ["init_on_alloc=0" "init_on_free=0"];
-        zfs.package = config.boot.kernelPackages.zfs_cachyos;
+        zfs.package = pkgs.cachyosKernels.zfs-cachyos-lto;
       };
     };
 
