f6b7c6e481aaa231f9c5b5f8950a726aea74c112
Cassandra Comar <ccomar@drwholdings.com>
2026-02-03 11:44:13 -0500

    install vscode-json-language-server

--- users/base/packages.nix
+++ users/base/packages.nix
@@ -7,38 +7,42 @@
   # Let Home Manager install and manage itself.
   # programs.home-manager.enable = true;
 
-  home.packages = with pkgs; [
-    slack
-    signal-desktop
-    keepassxc
-    google-chrome
-    discord
-    nodePackages.bash-language-server
-    spotify
-    curlFull
-    rclone
-    syncthing
-    dbus
-    terraform
-    xclip
-    lilypond
-    reaper
-    evtest
-    audacity
-    # pkgs.ocenaudio
-    android-studio
-    # ffmpeg-full
-    # svt-av1
-    # libopus
-    numactl
-    arandr
-    pinentry-rofi
-    openconnect
-    android-tools
-    fly
-    veracrypt
-    nixd
-  ];
+  home.packages = with pkgs;
+    [
+      slack
+      signal-desktop
+      keepassxc
+      google-chrome
+      discord
+      spotify
+      curlFull
+      rclone
+      syncthing
+      dbus
+      terraform
+      xclip
+      lilypond
+      reaper
+      evtest
+      audacity
+      # pkgs.ocenaudio
+      android-studio
+      # ffmpeg-full
+      # svt-av1
+      # libopus
+      numactl
+      arandr
+      pinentry-rofi
+      openconnect
+      android-tools
+      fly
+      veracrypt
+      nixd
+    ]
+    ++ (with pkgs.nodePackages; [
+      bash-language-server
+      vscode-json-languageserver
+    ]);
 
   home.file.".config/discord/settings.json".text = ''
     { "SKIP_HOST_UPDATE": true }
