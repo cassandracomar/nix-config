2ebe5febda35e6fab052d04621ed6616169e2861
Cassandra Comar <ccomar@drwholdings.com>
2026-02-04 13:49:16 -0500

    update patch sha

--- flake.nix
+++ flake.nix
@@ -114,7 +114,7 @@
           patches = [
             (prev.fetchpatch {
               url = "https://github.com/redhat-developer/yaml-language-server/pull/1169.patch";
-              sha256 = "sha256-g4uMv65qAK3ytx5HSL8hBCemdigagdlKRUmZErT9LV8=";
+              sha256 = "sha256-zjGug25df695nqdSGOQCn7cKxusgxqgoheBUaIOGQnQ=";
             })
           ];
         });
