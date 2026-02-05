1768b84f39772c3bc79c84a5556ae52c38e04495
Cassandra Comar <cass@ndra.io>
2026-02-05 12:45:41 -0500

    disable tlp

--- system/base/power-management.nix
+++ system/base/power-management.nix
@@ -4,15 +4,15 @@
   pkgs,
   ...
 }: {
-  services.tlp = {
-    enable = true;
-    settings = {
-      TLP_DEFAULT_MODE = "AC";
-      CPU_SCALING_GOVERNOR_ON_AC = "ondemand";
-      CPU_SCALING_GOVERNOR_ON_BAT = "schedutil";
-    };
-  };
-  services.acpid.enable = true;
-  powerManagement.enable = true;
-  powerManagement.powertop.enable = false;
+  # services.tlp = {
+  #   enable = true;
+  #   settings = {
+  #     TLP_DEFAULT_MODE = "AC";
+  #     CPU_SCALING_GOVERNOR_ON_AC = "ondemand";
+  #     CPU_SCALING_GOVERNOR_ON_BAT = "schedutil";
+  #   };
+  # };
+  # services.acpid.enable = true;
+  # powerManagement.enable = true;
+  # powerManagement.powertop.enable = false;
 }
