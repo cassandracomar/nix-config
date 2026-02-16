66866ea0622f8ffaa62f36ad77bd5df462fa1346
Cassandra Comar <cass@ndra.io>
2026-02-16 10:02:42 -0500

    use upstream opentype-feature-freezer

--- packages/iosevka.nix
+++ packages/iosevka.nix
@@ -1,11 +1,9 @@
 {
-  poetry2nix,
   iosevka,
   nerd-font-patcher,
-  fetchFromGitHub,
   stdenv,
-  applyPatches,
   parallel,
+  opentype-feature-freezer,
   ...
 }: let
   plainPackage = iosevka.override {
@@ -16,23 +14,6 @@
       exportGlyphNames = true;
     };
     set = "Custom";
-  };
-  feature-freezer-src = applyPatches {
-    src = fetchFromGitHub {
-      owner = "twardoch";
-      repo = "fonttools-opentype-feature-freezer";
-      rev = "2ae16853bc724c3e377726f81d9fc661d3445827";
-      sha256 = "sha256-mIWQF9LTVKxIkwHLCTVK1cOuiaduJyX8pyBZ/0RKIVE=";
-    };
-    patches = [
-      ./feature-freezer-poetry-fixes.patch
-    ];
-  };
-  feature-freezer = poetry2nix.mkPoetryApplication {
-    projectDir = feature-freezer-src;
-    src = feature-freezer-src;
-    checkGroups = [];
-    doCheck = false;
   };
 in {
   iosevka-custom = plainPackage;
@@ -47,9 +28,8 @@ in {
     buildPhase = ''
       find \( -name \*.ttf -o -name \*.otf \) -print0 | parallel -0 -P ''${NIX_BUILD_CORES} cd {//} '&&' nerd-font-patcher --complete --careful {/}
       find \( -name \*.ttf -o -name \*.otf \) -print0 | parallel -0 -P ''${NIX_BUILD_CORES} cd {//} '&&' chmod +x {/}
-      find \( -name "*NerdFont*.ttf" -o -name "*NerdFont*.otf" \) -print0 | parallel -0 -P ''${NIX_BUILD_CORES} -m ${feature-freezer}/bin/pyftfeatfreeze -rnv -f dlig
+      find \( -name "*NerdFont*.ttf" -o -name "*NerdFont*.otf" \) -print0 | parallel -0 -P ''${NIX_BUILD_CORES} -m ${opentype-feature-freezer}/bin/pyftfeatfreeze -rnv -f dlig
     '';
     installPhase = "cp -a . $out";
   };
-  pyftfeatfreeze = feature-freezer;
 }
