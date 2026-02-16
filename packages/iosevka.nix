14a4097db7c7dbe8ab4650761547e8dc8a85e7da
Cassandra Comar <cass@ndra.io>
2026-02-16 12:05:21 -0500

    don't bother freezing
    the result is the same in the end

--- packages/iosevka.nix
+++ packages/iosevka.nix
@@ -3,7 +3,6 @@
   nerd-font-patcher,
   stdenv,
   parallel,
-  python3,
   ...
 }: let
   plainPackage = iosevka.override {
@@ -15,18 +14,10 @@
     };
     set = "Custom";
   };
-  pyenv = python3.withPackages (ps:
-    with ps; [
-      fonttools
-      (opentype-feature-freezer.overrideAttrs (old: {
-        patches = (old.patches or []) ++ [./feature-freezer-fontTools.patch];
-      }))
-    ]);
-in rec {
-  fontToolsPyEnv = pyenv;
+in {
   iosevka-custom = plainPackage;
-  iosevka-nerd-font-pre = stdenv.mkDerivation {
-    pname = "iosevka-nerd-font-pre";
+  iosevka-nerd-font = stdenv.mkDerivation {
+    pname = "iosevka-nerd-font";
     version = plainPackage.version;
 
     src = plainPackage;
@@ -38,18 +29,9 @@ in rec {
       find \( -name \*.ttf -o -name \*.otf \) -print0 | parallel -0 -P ''${NIX_BUILD_CORES} cd {//} '&&' nerd-font-patcher --complete --careful {/}
       find \( -name \*.ttf -o -name \*.otf \) -print0 | parallel -0 -P ''${NIX_BUILD_CORES} cd {//} '&&' chmod +x {/}
     '';
-    installPhase = "cp -a . $out";
-  };
-  iosevka-nerd-font = stdenv.mkDerivation {
-    pname = "iosevka-nerd-font";
-    version = plainPackage.version;
-    src = iosevka-nerd-font-pre;
-
-    nativeBuildInputs = [parallel pyenv];
-
     installPhase = ''
-      mkdir -p $out/share/fonts/opentype
-      find -name "*NerdFont*.ttf" -exec sh -c 'echo $(dirname {})/$(basename {} .ttf)' \; | parallel -P ''${NIX_BUILD_CORES} -m pyftfeatfreeze -nv -f dlig {//}/{/}.ttf $out/share/fonts/opentype/{/}.otf
+      mkdir -p $out/share/fonts/truetype
+      find \( -name "*NerdFont*.ttf" \) -print0 | xargs -0 -I'{}' install -m644 {} $out/{}
     '';
   };
 }
