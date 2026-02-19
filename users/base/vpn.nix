86e27b6060cea33af01e098eb63a7072027f793e
Cassandra Comar <ccomar@drwholdings.com>
2026-02-19 12:22:51 -0500

    fix vpn-slice domain setting

--- users/base/vpn.nix
+++ users/base/vpn.nix
@@ -11,10 +11,11 @@
           url = "https://github.com/dlenski/vpn-slice/pull/158.patch";
           sha256 = "sha256-vPsG8An0CnZDMmdZ3d4eZucwafLzO3BE3RaRqqHM8KU=";
         })
+        ../../packages/vpn-slice.patch
       ];
   });
   launch-split = pkgs.writeShellScript "launch-split.sh" ''
-    ${vpn-slice}/bin/vpn-slice --no-fork --domains-vpn-dns drwholdings.com,drw --verbose --dump 10.0.0.0/8 2>&1 \
+    ${vpn-slice}/bin/vpn-slice --no-fork --domains-vpn-dns drwholdings.com,drw,us.drwholdings.com --verbose --dump 10.0.0.0/8 2>&1 \
     | sudo -u ${config.home.username} systemd-cat -t vpn-slice
   '';
   launch-vpn = pkgs.writeShellScript "launch-vpn.sh" ''
@@ -35,10 +36,6 @@
       sudo kill "$MAINPID" "$(cat "$XDG_RUNTIME_DIR"/openconnect.pid)"
     fi
   '';
-  fixup-dns = pkgs.writeShellScript "fixup-dns.sh" ''
-    sleep 5
-    sudo ${pkgs.systemd}/bin/resolvectl domain tun0 drwholdings.com drw us.drwholdings.com
-  '';
 in {
   systemd.user.services.anyconnect = {
     Unit = {
@@ -49,22 +46,6 @@ in {
       PIDFile = "openconnect.pid";
       ExecStart = "${launch-vpn}";
       ExecStop = "${kill-vpn}";
-    };
-  };
-  systemd.user.services.vpn-dns = {
-    Unit = {
-      Description = "fixup vpn dns";
-      After = ["anyconnect.service"];
-      BindsTo = ["anyconnect.service"];
-    };
-
-    Service = {
-      Type = "oneshot";
-      ExecStart = "${fixup-dns}";
-    };
-
-    Install = {
-      WantedBy = ["anyconnect.service"];
     };
   };
 }
