f0c71ff58d99a322e77020f44366709c2ae28c1b
Cassandra Comar <cass@mountclare.net>
2026-02-24 11:11:51 -0500

    reduce max arc size
    we keep ooming because zfs won't free the cache

--- machines/yew.nix
+++ machines/yew.nix
@@ -21,7 +21,7 @@
   boot.kernelParams = ["nr_hugepages=4096"];
   boot.extraModulePackages = [config.boot.kernelPackages.it87];
   boot.extraModprobeConfig = ''
-    options zfs l2arc_headroom=0 metaslab_lba_weighting_enabled=0 zfs_vdev_sync_write_min_active=10  zfs_vdev_sync_write_max_active=32  zfs_vdev_async_write_min_active=10  zfs_vdev_async_write_max_active=32 l2arc_headroom=0 l2arc_rebuild_enabled=1 l2arc_exclude_special=0 l2arc_trim_ahead=20
+    options zfs l2arc_headroom=0 metaslab_lba_weighting_enabled=0 zfs_vdev_sync_write_min_active=10  zfs_vdev_sync_write_max_active=32  zfs_vdev_async_write_min_active=10  zfs_vdev_async_write_max_active=32 l2arc_headroom=0 l2arc_rebuild_enabled=1 l2arc_exclude_special=0 l2arc_trim_ahead=20 zfs_arc_max=51539607552
   '';
   # boot.zfs.extraPools = ["data"];
   boot.loader = {
