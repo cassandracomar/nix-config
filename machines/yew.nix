0764a1523bc837f023fb56ff56e5c75a71505bcf
Cassandra Comar <cass@mountclare.net>
2026-03-02 08:50:35 -0500

    maybe it's not numpad

--- machines/yew.nix
+++ machines/yew.nix
@@ -123,8 +123,8 @@
       ids = ["1532:00a7:58ecc9da" "1532:00a7:689c585f" "1532:00a7:58ecc9da"];
       settings = {
         main = {
-          "kp6" = "mouseforward";
-          "kp4" = "mouseback";
+          "6" = "mouseforward";
+          "4" = "mouseback";
         };
       };
     };
