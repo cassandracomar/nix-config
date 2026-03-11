3b4339ab5123f6fd59734437cfc5a9ff0e5d911c
Cassandra Comar <cass@mountclare.net>
2026-03-11 16:36:37 -0400

    change the default model

--- users/base/llm.nix
+++ users/base/llm.nix
@@ -3,7 +3,7 @@
   programs.claude-code = {
     enable = true;
     settings = {
-      model = "qwen3-coder-next:80b-128k";
+      model = "qwen3.5:27b";
       env = {
         ANTHROPIC_BASE_URL = "http://yew.local:11434";
         ANTHROPIC_AUTH_TOKEN = "ollama";
