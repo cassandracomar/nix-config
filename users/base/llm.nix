e6e51655186fb02779ba2ccd18adeefb1649fdcd
Cassandra Comar <cass@mountclare.net>
2026-03-11 14:22:22 -0400

    remove api key

--- users/base/llm.nix
+++ users/base/llm.nix
@@ -5,7 +5,6 @@
       model = "qwen3-coder-next:80b-128k";
       env = {
         ANTHROPIC_BASE_URL = "https://yew.local:11134";
-        ANTHROPIC_API_KEY = "";
         ANTHROPIC_AUTH_TOKEN = "ollama";
       };
     };
