cfd7911850df5f9bbf693ad74d8850b0f4865a7a
Cassandra Comar <cass@mountclare.net>
2026-03-11 15:21:27 -0400

    fix base url

--- users/base/llm.nix
+++ users/base/llm.nix
@@ -5,7 +5,7 @@
     settings = {
       model = "qwen3-coder-next:80b-128k";
       env = {
-        ANTHROPIC_BASE_URL = "https://yew.local:11134";
+        ANTHROPIC_BASE_URL = "http://yew.local:11434";
         ANTHROPIC_AUTH_TOKEN = "ollama";
       };
     };
