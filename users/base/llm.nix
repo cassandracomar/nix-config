07ccdaacced2ba338c0e4ebc56e895f4fa7fc9c0
Cassandra Comar <cass@mountclare.net>
2026-03-13 11:13:29 -0400

    unset auth token

--- users/base/llm.nix
+++ users/base/llm.nix
@@ -6,6 +6,7 @@
       env = {
         ANTHROPIC_BASE_URL = "http://yew.local:8001";
         ANTHROPIC_API_KEY = "sk-no-key-required";
+        ANTHROPIC_AUTH_TOKEN = "";
         CLAUDE_CODE_ENABLE_TELEMETRY = "0";
         CLAUDE_CODE_DISABLE_NONESSENTIAL_TRAFFIC = "1";
         CLAUDE_CODE_ATTRIBUTION_HEADER = "0";
