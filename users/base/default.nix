b85035de10858314e7cdc538b1b5a2c22057e894
Cassandra Comar <ccomar@drwholdings.com>
2026-05-27 11:09:26 -0400

    fix cargo

--- users/base/default.nix
+++ users/base/default.nix
@@ -10,14 +10,29 @@
     ./kubernetes.nix
     ./mail.nix
     ./packages.nix
-    ./rust.nix
+    # ./rust.nix
     ./shell-cfg.nix
     ./pinnacle.nix
     # ./xmonad.nix
     ./vpn.nix
   ];
 
-  home.packages = with pkgs; [rustc cargo];
+  home.packages = with pkgs; [
+    rustc
+    (pkgs.symlinkJoin {
+      name = "cargo-${cargo.version}";
+      inherit (cargo) version meta;
+      paths = [cargo];
+      postBuild = ''
+        completion="$out/share/fish/vendor_completions.d/cargo.fish"
+        if [ -L "$completion" ]; then
+          rm "$completion"
+          cp ${cargo}/share/fish/vendor_completions.d/cargo.fish "$completion"
+          sed -i "s|/nix/store/[a-z0-9]\{32\}-cargo-bootstrap[^/]*|${cargo}|g" "$completion"
+        fi
+      '';
+    })
+  ];
   xdg.userDirs.setSessionVariables = true;
   gtk.gtk4.theme = config.gtk.theme;
 }
