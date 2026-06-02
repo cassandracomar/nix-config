3318ffd5a734b95096a04d4d77298c9020ee92e4
Cassandra Comar <cass@mountclare.net>
2026-06-02 11:08:10 -0400

    regenerate profile

--- users/cassandra.nix
+++ users/cassandra.nix
@@ -56,8 +56,8 @@
         "-march=znver4"
         "-mtune=znver4"
         "-flto=thin"
-        # "-fcs-profile-generate"
-        "-fprofile-use=${profdata}"
+        "-fcs-profile-generate"
+        # "-fprofile-use=${profdata}"
       ]
       ++ old.NIX_CFLAGS_COMPILE or []
     );
