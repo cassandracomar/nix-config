efddcf692d09b3ba7b61477e7e6bc39a06a2a1e7
Cassandra Comar <ccomar@drwholdings.com>
2026-06-05 12:34:05 -0400

    try setting spacing = term

--- packages/iosevka.nix
+++ packages/iosevka.nix
@@ -3,18 +3,23 @@
   callPackage,
   ...
 }: let
+  privateBuildPlan = {
+    family = "Iosevka";
+    ligations.inherits = "dlig";
+    ligations.enables = ["slasheq"];
+    exportGlyphNames = true;
+  };
   iosevka-custom = iosevka.override {
-    privateBuildPlan = {
-      family = "Iosevka";
-      ligations.inherits = "dlig";
-      ligations.enables = ["slasheq"];
-      exportGlyphNames = true;
-    };
+    inherit privateBuildPlan;
+    set = "-Custom";
+  };
+  iosevka-custom-mono = iosevka.override {
+    privateBuildPlan = privateBuildPlan // {spacing = "term";};
     set = "-Custom";
   };
   nerdFontPatcher = callPackage ./nerdFontPatcher.nix {};
 in {
   inherit iosevka-custom;
   iosevka-nerd-font = nerdFontPatcher iosevka-custom {};
-  iosevka-nerd-font-mono = nerdFontPatcher iosevka-custom {mono = true;};
+  iosevka-nerd-font-mono = nerdFontPatcher iosevka-custom-mono {mono = true;};
 }
