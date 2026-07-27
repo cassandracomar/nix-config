6d46c591a9383d5cb91cdc31fc05013b08f3e220
Cassandra Comar <ccomar@drwholdings.com>
2026-07-27 10:18:41 -0400

    update nvidia dependency

--- users/ccomar.nix
+++ users/ccomar.nix
@@ -171,8 +171,8 @@ in {
       enable = true;
       nvidia = {
         enable = true;
-        version = "595.71.05";
-        sha256 = "sha256-NiA7iWC35JyKQva6H1hjzeNKBek9KyS3mK8G3YRva4I=";
+        version = "595.84";
+        sha256 = "sha256-mcQE5SExvye8ptoCaNzOPr7cenOrF0BxqZXPGmxeugY=";
       };
     };
   };
