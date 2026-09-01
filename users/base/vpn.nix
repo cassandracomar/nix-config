c7f8dfc382ae42bb783285281e830a00696affda
Cassandra Comar <ccomar@drwholdings.com>
2026-09-01 14:34:09 -0400

    cisco secureclient vpn

--- users/base/vpn.nix
+++ users/base/vpn.nix
@@ -1,44 +1,9 @@
-{
-  pkgs,
-  config,
-  ...
-}: let
-  vpn-slice = pkgs.vpn-slice.overrideAttrs (old: {
-    patches =
-      (old.patches or [])
-      ++ [
-        (pkgs.fetchpatch {
-          url = "https://github.com/dlenski/vpn-slice/pull/158.patch";
-          sha256 = "sha256-vPsG8An0CnZDMmdZ3d4eZucwafLzO3BE3RaRqqHM8KU=";
-        })
-        ../../packages/vpn-slice.patch
-      ];
-  });
-  launch-split = pkgs.writeShellScript "launch-split.sh" ''
-    ${vpn-slice}/bin/vpn-slice --no-fork \
-      --domains-vpn-dns drwholdings.com,drw,us.drwholdings.com \
-      --no-host-names \
-      --no-ns-hosts \
-      --verbose \
-      --dump \
-      10.0.0.0/8 2>&1 \
-    | sudo -u ${config.home.username} systemd-cat -t vpn-slice
-  '';
+{pkgs, ...}: let
   launch-vpn = pkgs.writeShellScript "launch-vpn.sh" ''
-    ${pkgs.rbw}/bin/rbw get drwholdings.com ccomar \
-    | sudo ${pkgs.openconnect}/bin/openconnect --pid-file "$XDG_RUNTIME_DIR"/openconnect.pid \
-        --user=ccomar \
-        -g linux \
-        --csd-wrapper=${pkgs.openconnect}/libexec/openconnect/csd-post.sh \
-        --local-hostname=nylld-ccomar1 \
-        -s ${launch-split} \
-        nyvpn1.drwholdings.com \
-    | systemd-cat -t anyconnect
+    (echo "ccomar" && ${pkgs.rbw}/bin/rbw get drwholdings.com && echo "y") | /opt/cisco/secureclient/bin/vpn -s connect "New York - Linux"
   '';
   kill-vpn = pkgs.writeShellScript "kill-vpn.sh" ''
-    if [[ -f "$XDG_RUNTIME_DIR"/openconnect.pid ]]; then
-      sudo kill "$MAINPID" "$(cat "$XDG_RUNTIME_DIR"/openconnect.pid)"
-    fi
+    /opt/cisco/secureclient/bin/vpn disconnect
   '';
 in {
   systemd.user.services.anyconnect = {
@@ -47,9 +12,10 @@ in {
     };
 
     Service = {
-      PIDFile = "openconnect.pid";
+      Type = "oneshot";
       ExecStart = "${launch-vpn}";
       ExecStop = "${kill-vpn}";
+      RemainAfterExit = true;
     };
   };
 }
